942df796efadb68b4eb1fdd13bf597cb162e88d0
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2020-08-18 18:54:29 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import ast
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+
+with open('x-db-api/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+
+requirements = [
+    'bs4>=4.9.1',    
+]
+
+setup(
+    name='x-db-api',
+    version=version,
+    description="x-db-api",
+    long_description=readme,
+    author=["Sven Skender", "Ibrahim Ipek"]
+    author_email='-',
+    url='https://github.com/anbuhckr/x-db-api',
+    packages=find_packages(),
+    package_dir={},    
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='x-db-api',
+    classifiers=[
+        'Development Status :: 3 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: BSD License',
+        'Natural Language :: English',        
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Browsers'
+    ],
+)
